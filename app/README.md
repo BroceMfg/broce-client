e62089f338e03073379f57bb6a0ee131170541f7
andrew-ware <andrew.ware@gordon.edu>
2016-11-07 14:26:34 -0500

    Add react app with basic scaffolding

---
+++ app/README.md
@@ -0,0 +1,2 @@
+# broce-client app
+A React.js app built for the Broce Mfg. parts ordering system
---
+++ app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <span>hello world</span>
+      </div>
+    );
+  }
+}
+
+export default App;
