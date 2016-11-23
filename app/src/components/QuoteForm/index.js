6ee64a13123eb1cc45a5f56b3662e83299718a8a
andrew-ware <andrew.ware@gordon.edu>
2016-11-23 10:58:09 -0500

    Add new mach num block functionality working properly

--- app/src/components/QuoteForm/index.js
+++ app/src/components/QuoteForm/index.js
@@ -2,14 +2,57 @@ import React from 'react';
 import _ from 'lodash';
 import Input from '../Input';
 
+class MachineNumberBlock extends React.Component {
+  render() {
+    return (
+      <div className="MachineNumberBlock">
+        <span>MachineNumberBlock with index = {this.props.index}</span>
+      </div>
+    )
+  }
+}
+
 class QuoteForm extends React.Component {
   constructor(props) {
     super(props);
     this.form = {};
+    this.addMachNumBlock = this.addMachNumBlock.bind(this);
     this.submit = this.submit.bind(this);
+
+    // form structure should be like:
+    // {
+    //   <machineNumberBlockIndex>: {
+    //     <currentChoice>: {
+    //       <partNumberBlockIndex>: {
+    //         <currentChoice>: <quantity>
+    //       }
+    //     }
+    //   }
+    // }
+    // where there can be any number of machineNumber or partNumber blocks
+    this.initialMachNumBlock = { 'default': {} };
+    const initialForm = { 0: this.initialMachNumBlock };
+
     this.state = {
-      form: this.props.form ? _.clone(this.props.form) : {}
+      form: this.props.form ? _.clone(this.props.form) : initialForm
     }
+  }
+
+  addMachNumBlock(e) {
+    // prevent the button default so it doesn't submit the form on us
+    e.preventDefault();
+    console.log('addMachNumBlock function called');
+    const formKeys = Object.keys(this.state.form);
+    const newMachNumBlock = {};
+    newMachNumBlock[parseInt(formKeys[formKeys.length - 1]) + 1] = this.initialMachNumBlock;
+    const newState = {
+      ...this.state,
+      form: Object.assign(
+        this.state.form,
+        newMachNumBlock
+      )
+    };
+    this.setState(newState);
   }
 
   submit(e) {
@@ -23,16 +66,27 @@ class QuoteForm extends React.Component {
   }
 
   render() {
+    const form = this.state.form;
     return (
       <div className="OrderForm">
         <h1>Quote Form</h1>
         <form onSubmit={this.submit}>
-          <Input 
+          {/*<Input 
             refProp={(input) => { this.form.name = input }}
             type="text"
             name="name"
             placeholder="Name"
-            value={'Andrew'} />
+            value={'Andrew'}
+          />*/}
+          {
+            Object.keys(form).map(key => (
+              <MachineNumberBlock
+                key={key}
+                index={key}
+              />
+            ))
+          }
+          <button onClick={this.addMachNumBlock}>Add Another Machine Number</button>
           <button type="submit">Submit</button>
         </form>
       </div>
