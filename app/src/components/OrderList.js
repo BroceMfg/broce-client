c469b8f8aab8cd1b663e4166a227394da552c27a
andrew-ware <andrew.ware@gordon.edu>
2017-01-26 16:42:25 -0500

    Add "no orders to display" message when there are no orders

--- app/src/components/OrderList.js
+++ app/src/components/OrderList.js
@@ -51,22 +51,30 @@ class OrderList extends React.Component {
     }
     console.log(list);
     let keyCount = 1;
-    return (
-      list.map((orders, i) => (
-        <OrderSubList
-          key={keyCount++}
-          admin={this.props.admin}
-          apiUrl={this.props.apiUrl}
-          orders={orders}
-          updateOrder={this.updateOrder}
-          promoteOrder={this.promoteOrder}
-          statusType={getStatusType(this.props.orders, i)}
-          getStatusType={this.props.getStatusType}
-          getNextStatusType={this.getNextStatusType}
-          toggleMessage={this.props.toggleMessage}
-        />
-      ))
-    );
+    return Object.keys(list) > 0
+      ? (
+          list.map((orders, i) => (
+            <OrderSubList
+              key={keyCount++}
+              admin={this.props.admin}
+              apiUrl={this.props.apiUrl}
+              orders={orders}
+              updateOrder={this.updateOrder}
+              promoteOrder={this.promoteOrder}
+              statusType={getStatusType(this.props.orders, i)}
+              getStatusType={this.props.getStatusType}
+              getNextStatusType={this.getNextStatusType}
+              toggleMessage={this.props.toggleMessage}
+            />
+          ))
+        )
+    : (
+      <div className="OrderSubList">
+        <div>
+          <h3>Currently, there are no orders to display.</h3>
+        </div>
+      </div>
+    )
   }
 
   render() {
