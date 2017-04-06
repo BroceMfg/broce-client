e018a3263bba018242032f3477bc626b7d50ad81
andrew-ware <andrew.ware@gordon.edu>
2017-04-05 20:43:09 -0400

    Add better error handling in ErrorHandler component + styles

--- src/components/App/misc/ErrorHandler.js
+++ src/components/App/misc/ErrorHandler.js
@@ -1,17 +1,28 @@
 import React, { PropTypes } from 'react';
+import '../../../css/components/ErrorHandler.css';
 
 const ErrorHandler = props => (
   <div className="ErrorHandler">
     <div className="inner">
       <div className="oops-header-wrapper">
-        <h1>Oops... It appears an error occurred.</h1>
+        <h1 className="oops-header">Oops... It appears an error occurred.</h1>
       </div>
       <div className="err-msg-wrapper">
         {
           props.log && props.msg
             ? <span>{props.msg}</span>
-            : <span>{props.def}</span>
+            : <span>{props.defErrMsg}</span>
         }
+      </div>
+      <div className="return-home">
+        <button
+          onClick={() => {
+            localStorage.removeItem('state');
+            window.location = '/';
+          }}
+        >
+          Return Home
+        </button>
       </div>
     </div>
   </div>
@@ -25,6 +36,6 @@ ErrorHandler.propTypes = {
   defErrMsg: PropTypes.string
 };
 
-ErrorHandler.defaultPropTypes = {
+ErrorHandler.defaultProps = {
   defErrMsg: 'An Unexpected Error Occurred.'
 };
