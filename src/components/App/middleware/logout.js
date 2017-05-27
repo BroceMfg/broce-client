2c1366c906af46dc2bba177598ce86a2f70a4176
andrew-ware <andrew.ware@gordon.edu>
2017-05-27 12:53:59 -0400

    Add minor bug fixes

--- src/components/App/middleware/logout.js
+++ src/components/App/middleware/logout.js
@@ -14,5 +14,7 @@ module.exports = function logout() {
     }
   );
   // refresh the broswer to unmount/remount our component
-  window.location = '/';
+  setTimeout(() => {
+    window.location = '/';
+  }, 100);
 };
